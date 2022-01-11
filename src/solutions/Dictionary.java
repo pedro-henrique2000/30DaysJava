62d38fe7a3c720e9bbbc38180a15126587dabc86
phb2000 <p.becker2000@hotmail.com>
2022-01-11 18:23:08 -0300

    dictionaries and maps exercise

---
+++ src/solutions/Dictionary.java
@@ -0,0 +1,26 @@
+package solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Dictionary {
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            map.put(name, phone);
+        }
+        while(in.hasNext()){
+            String s = in.next();
+            if (map.containsKey(s))
+                System.out.println(String.format("%s=%d", s, map.get(s)));
+            else
+                System.out.println("Not found");
+        }
+        in.close();
+    }
+}
