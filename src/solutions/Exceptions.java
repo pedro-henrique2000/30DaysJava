2456d7ee638e3105ca32f64ff9e9fd37c022f32a
phb2000 <p.becker2000@hotmail.com>
2022-01-20 17:03:55 -0300

    exception exercise

---
+++ src/solutions/Exceptions.java
@@ -0,0 +1,16 @@
+package solutions;
+
+public class Exceptions {
+
+    public static void main(String[] args) {
+        String s = "123";
+
+        try {
+            System.out.println(Integer.parseInt(s))
+            ;
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+    }
+
+}
