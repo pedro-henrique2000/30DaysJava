063020a3c3dbeb0a7b51b889ceede2a85860ec98
phb2000 <p.becker2000@hotmail.com>
2022-01-24 17:28:46 -0300

    bubble exercise

---
+++ src/solutions/Sorting.java
@@ -0,0 +1,61 @@
+package solutions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Sorting {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int bubble = bubble(n, a);
+
+        System.out.printf("Array is sorted in %d swaps.%n", bubble);
+        System.out.printf("First Element: %d%n", a.get(0));
+        System.out.printf("Last Element: %d%n", a.get(a.size() - 1));
+
+
+        bufferedReader.close();
+    }
+
+    private static int bubble(int n, List<Integer> a) {
+        int numberOfSwaps = 0;
+        for (int i = 0; i < n; i++) {
+            // Track number of elements swapped during a single array traversal
+            ;
+
+            for (int j = 0; j < n - 1; j++) {
+                // Swap adjacent elements if they are in decreasing order
+                if (a.get(j) > a.get(j + 1)) {
+
+                    swap(a, j);
+                    numberOfSwaps++;
+                }
+            }
+
+            // If no elements were swapped during a traversal, array is sorted
+            if (numberOfSwaps == 0) {
+                break;
+            }
+        }
+
+        return numberOfSwaps;
+    }
+
+    private static void swap(List<Integer> a, int j) {
+        int temp = a.get(j);
+        a.set(j, a.get(j + 1));
+        a.set(j + 1, temp);
+    }
+
+}
